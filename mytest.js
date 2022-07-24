47cd4f334c975138492f2289c63fd3b5f1f55379
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-07-24 11:25:44 +0300

    feat: add solution №2

--- mytest.js
+++ mytest.js
@@ -1,5 +1,12 @@
-function isPrime(n) {
-  return !!(n % 2);
+function swapHeadAndTail(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  return arr.length % 2 === 0
+    ? arr.splice((arr.length / 2)).concat(arr.splice(-(arr.length / 2), Math.floor(arr.length / 2)))
+    : arr.splice(Math.ceil(arr.length / 2))
+      .concat(arr[Math.floor(arr.length / 2)])
+      .concat(arr.splice(-(Math.ceil(arr.length / 2)) - 1, Math.ceil(arr.length / 2)));
 }
 
-console.log(isPrime(11));
+console.log(swapHeadAndTail([1, 2, 3, 4]));
